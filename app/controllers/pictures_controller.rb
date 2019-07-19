2b35b030c4fdef6f58ab1c83ff75cd1c31142b09
Takashi <Takashi@gmail.com>
2019-07-19 17:36:06 +0900

    メール投稿機能を実装しました

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -28,7 +28,8 @@ class PicturesController < ApplicationController
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        PictureMailer.picture_mail(@picture).deliver
+        format.html { redirect_to @picture, notice: '記事を投稿しました、確認用メールを送りました' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,2 +1,7 @@
 class PictureMailer < ApplicationMailer
+  def picture_mail(picture)
+    @picture = picture
+
+    mail to: "t757f37a@gmail.com", subject: "お問い合わせの確認メール"
+  end
 end
