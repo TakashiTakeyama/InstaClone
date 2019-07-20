8605892262ad0538979cd201b9a7c30ef2aa1927
Takashi <Takashi@gmail.com>
2019-07-20 13:18:44 +0900

    favoriteモデルとコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
      post :confirm
     end
   end
+  resources :favorites, only: %i[create destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
