65ac10d66ed13ead498ddb23afd4fdd08b0f78b0
Takashi <Takashi@gmail.com>
2019-07-23 13:40:48 +0900

    Pictureモデルにvalidateを追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  validates :image_cache, presence: true
+  validates :content, presence: true
 end
